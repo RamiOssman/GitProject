ba5ca233a9bcd3e1a145ac97844dcce7f7647155
RamiOssman <rami-ossma@live.com>
2017-08-01 21:07:08 +0200

    Local commit

--- app/src/main/java/m5/gitproject/MainActivity.java
+++ app/src/main/java/m5/gitproject/MainActivity.java
@@ -2,6 +2,7 @@ package m5.gitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+     Toast t =  new Toast(this);
+        t.setDuration(Toast.LENGTH_LONG);
+        t.setText("Text");
+        t.show();
     }
 }
