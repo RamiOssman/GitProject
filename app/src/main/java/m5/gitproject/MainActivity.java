00cfa322c5df71dc016a122a5bd191677163ca86
RamiOssman <rami-ossma@live.com>
2017-08-01 21:18:48 +0200

    ok

--- app/src/main/java/m5/gitproject/MainActivity.java
+++ app/src/main/java/m5/gitproject/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-     Toast t =  new Toast(this);
-        t.setDuration(Toast.LENGTH_LONG);
+     Toast t = Toast.makeText(this , "" , Toast.LENGTH_LONG);
         t.setText("How are you");
         t.show();
     }
